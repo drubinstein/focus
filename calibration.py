cc816352a50e728540cd329d15f9dd5ab6d8a50a
David Rubinstein <drubinstein91@gmail.com>
2016-08-28 13:23:16 -0400

    Adding tensorflow implementation of calibration and first attempts at running gaze detection

--- calibration.py
+++ calibration.py
@@ -6,10 +6,46 @@ import Tkinter as tk
 import time
 from random import randint
 
-from lasagne import layers
-from lasagne.updates import nesterov_momentum
-from nolearn.lasagne import NeuralNet
+import lasagne
 import matplotlib.pyplot as pyplot
+
+import theano
+import theano.tensor as T
+
+from six.moves import cPickle
+
+NUM_HIDDEN_UNITS=30
+BATCH_SIZE=1
+
+def build_model(frame_w, frame_h, output_dim,
+                batch_size=BATCH_SIZE, num_hidden_units=NUM_HIDDEN_UNITS, input_var=None):
+    """Create a symbolic representation of the neural network with
+    input_dim input nodes
+    output_dim output nodes
+    num_hidden_units per hidden layer
+    input_var symbolic input variable
+
+    return: A theano expression representing the network
+    """
+    l_in = lasagne.layers.InputLayer(shape=(None, 1, frame_h,frame_w), input_var=input_var)
+    l_hidden1 = lasagne.layers.DenseLayer(
+        l_in,
+        num_units=num_hidden_units,
+        nonlinearity=lasagne.nonlinearities.rectify,
+    )
+    l_hidden2 = lasagne.layers.DenseLayer(
+        l_hidden1,
+        num_units=num_hidden_units,
+        nonlinearity=lasagne.nonlinearities.rectify,
+    )
+    l_out = lasagne.layers.DenseLayer(
+        l_hidden2,
+        num_units=output_dim,
+        nonlinearity=lasagne.nonlinearities.linear,
+        b=None,
+    )
+    return l_out
+
 
 def main():
     root = tk.Tk()
@@ -33,32 +69,21 @@ def main():
     print "Frame dims are '{0}' x '{1}'".format(frame_w,frame_h)
 
     print('Initializing neural net')
-    net1 = NeuralNet(
-        layers=[  # three layers: two hidden layers
-            ('input', layers.InputLayer),
-            ('hidden', layers.DenseLayer),
-            ('hidden', layers.DenseLayer),
-            ('output', layers.DenseLayer),
-            ],
-        # layer parameters:
-        input_shape=(None, frame_h*frame_w),
-        hidden_num_units=100,  # number of units in hidden layer
-        output_nonlinearity=None,  # output layer uses identity function
-        output_num_units=2,  #  target values
-
-        # optimization method:
-        update=nesterov_momentum,
-        update_learning_rate=0.01,
-        update_momentum=0.9,
-
-        regression=True,  # flag to indicate we're dealing with regression problem
-        max_epochs=400,  # we want to train this many epochs
-        verbose=1,
-        )
+    input_var = T.tensor4('inputs')
+    target_var = T.ivector('targets')
+    network = build_model(frame_w, frame_h, 2, 1, 30, input_var)
+    prediction = lasagne.layers.get_output(network)
+    predict_fn = theano.function([input_var], prediction)
+    #loss = lasagne.objectives.categorical_crossentropy(prediction, target_var)
+    #loss = loss.mean()
+    loss = T.sum(lasagne.objectives.squared_error(target_var,prediction))
+    params = lasagne.layers.get_all_params(network, trainable=True)
+    updates = lasagne.updates.nesterov_momentum(loss, params, learning_rate=0.01, momentum=0.9)
+    train_fn = theano.function([input_var, target_var], loss, updates=updates)
 
     print('generating 25 random points to choose from')
-    npts = 25
-    for pt in xrange(0,25):
+    npts = 5
+    for _ in xrange(0,npts):
         img[:] = (0,0,0) # clear
 
         x = randint(0,screen_width-1)
@@ -68,10 +93,11 @@ def main():
         target = np.array([[np.float32(x), np.float32(y)]])
         #create a white circle at the randomly selected point
         cv2.circle(img, (x,y), 10, (255,255,255), -1)
-        cv2.imshow('calibration', img)
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+        cv2.waitKey(100)
+        cv2.imshow('calibration', img)
+        cv2.waitKey(100)
+
 
         #capture for 5 seconds
         t_end = time.time() + 5
@@ -82,10 +108,43 @@ def main():
             # Otherwise we'd just take the luminance values from the frame read
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             gray_sc = np.float32(gray / 255.)
-            gray_rs = np.reshape(gray_sc,(1,frame_h*frame_w))
 
-            train_fn = theano.function([
-            net1.fit(gray_rs,np.array([[np.float32(x),np.float32(y)]]))
+            #Now go train!
+            coord=np.int32(np.array([x,y]))
+            train_fn([[gray_sc]], coord)
+
+    print('Saving the model to focus.mdl')
+    f = open('focus.mdl', 'wb')
+    cPickle.dump(network, f, protocol = cPickle.HIGHEST_PROTOCOL)
+    f.close()
+
+    print('Now continuing onto testing')
+    img = np.zeros((screen_height, screen_width,3), np.uint8)
+
+    while(True):
+        #Capture frame by frame
+        ret, frame = cap.read()
+
+        # Operate on the frame
+        # We're not doing anything special so grayscale should be good enough
+        # Otherwise we'd just take the luminance values from the frame read
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        #cut off the left and right sides (cause wide screen is stupid)
+        gray_sc = np.float32(gray / 255.)
+
+
+        y_pred = lasagne.layers.get_output(network, inputs=gray_sc)
+        print eval(y_pred)
+
+
+        cv2.imshow('GrayFrame',gray)
+        img[:] = (0,0,0) # clear
+        cv2.circle(img, y_pred, 10, (255,255,255), -1)
+        #Display the resulting frame
+        plt.show(block=False)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
 if __name__ == '__main__':
     main()
---
+++ calibration_tf.py
@@ -0,0 +1,150 @@
+#file calibration.py
+import os
+import cv2
+import numpy as np
+import Tkinter as tk
+import time
+from random import randint
+
+import matplotlib.pyplot as pyplot
+import tensorflow as tf
+
+
+from six.moves import cPickle
+
+NUM_HIDDEN_UNITS=100
+BATCH_SIZE=1
+printing=False
+
+def mlp(x, weights, biases):
+    #Hidden Layer with RELU activation
+    layer_1 = tf.tanh(tf.add(tf.matmul(x, weights['h1']), biases['b1']))
+    #layer_1 = tf.nn.relu(layer_1)
+    if printing: layer_1 = tf.Print(layer_1, [layer_1], 'layer 1: ', summarize=NUM_HIDDEN_UNITS)
+    #Hidden Layer with RELU activation
+    layer_2 = tf.tanh(tf.add(tf.matmul(layer_1, weights['h2']), biases['b2']))
+    #layer_2 = tf.nn.relu(layer_2)
+    if printing: layer_2 = tf.Print(layer_2, [layer_2], 'layer 2: ', summarize=NUM_HIDDEN_UNITS)
+    #Output layer
+    out_layer = tf.matmul(layer_2, weights['out']) + biases['out']
+    if printing: out_layer = tf.Print(out_layer,[out_layer], 'output layer: ')
+    return out_layer
+
+def main():
+    root = tk.Tk()
+
+#use 1/2 screen width because I personally use dual monitors
+    screen_width = root.winfo_screenwidth()/4
+    screen_height = root.winfo_screenheight()/2
+
+    img = np.zeros((screen_height, screen_width,3), np.uint8)
+    print screen_width, screen_height
+    fs = 100
+#TODO: Instead of looping through every w and h instead choose 25 random points and
+#get captures for those with half-second delays
+
+    print('starting video')
+    cap = cv2.VideoCapture(0)
+    #get video resolution
+    ret, frame = cap.read()
+    print frame.shape
+    frame_h, frame_w, _ = frame.shape
+    npxls = frame_h*frame_w
+    print "Frame dims are '{0}' x '{1}'".format(frame_w,frame_h)
+
+    print('Initializing neural net')
+    learning_rate = 0.001
+    n_input = npxls
+    n_hidden_1 = NUM_HIDDEN_UNITS
+    n_hidden_2 = NUM_HIDDEN_UNITS
+    n_out = 2
+    X = tf.placeholder(tf.float32, [None, frame_h*frame_w])
+    Y = tf.placeholder(tf.float32, [None, n_out])
+    # Store layers weight & bias
+    weights = {
+        'h1': tf.Variable(tf.random_normal([n_input, n_hidden_1])),
+        'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2])),
+        'out': tf.Variable(tf.random_normal([n_hidden_2, n_out]))
+        }
+    biases = {
+        'b1': tf.Variable(tf.random_normal([n_hidden_1])),
+        'b2': tf.Variable(tf.random_normal([n_hidden_2])),
+        'out': tf.Variable(tf.random_normal([n_out]))
+        }
+
+    #create multilayer perceptron
+    pred = mlp(X,weights, biases)
+
+    #define cost function
+    cost = tf.reduce_sum(tf.pow(pred-Y,2))/2
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
+    init = tf.initialize_all_variables()
+
+    with tf.Session() as sess:
+        sess.run(init)
+
+        npts = 5
+        print "generating '{0}' random points to choose from".format(npts)
+        for _ in xrange(0,npts):
+            img[:] = (0,0,0) # clear
+
+            x = randint(0,screen_width-1)#/screen_width
+            y = randint(0,screen_height-1)#/screen_height
+            print x,y
+
+            target = np.array([[np.float32(x), np.float32(y)]])
+            #create a white circle at the randomly selected point
+            cv2.circle(img, (x,y), 10, (255,255,255), -1)
+
+            cv2.waitKey(100)
+            cv2.imshow('calibration', img)
+            cv2.waitKey(100)
+
+
+            #capture for 5 seconds
+            t_end = time.time() + 5
+            while time.time() < t_end:
+                ret, frame = cap.read()
+                # Operate on the frame
+                # We're not doing anything special so grayscale should be good enough
+                # Otherwise we'd just take the luminance values from the frame read
+                gray = np.float32(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY))
+                gray_rs = np.reshape(gray,(1,n_input))
+                #gray_sc = np.float32(gray / 255.)
+
+                #Now go train!
+                sess.run(optimizer, feed_dict={X: gray_rs, Y: [[x,y]]})
+
+
+        print('Now continuing onto testing')
+        img = np.zeros((screen_height, screen_width,3), np.uint8)
+
+        while(True):
+            #Capture frame by frame
+            ret, frame = cap.read()
+
+            # Operate on the frame
+            # We're not doing anything special so grayscale should be good enough
+            # Otherwise we'd just take the luminance values from the frame read
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+            #cut off the left and right sides (cause wide screen is stupid)
+            gray_sc = np.float32(gray)
+            gray_rs = np.reshape(gray,(1,n_input))
+
+            feed_dict = {X: gray_rs}
+            p = sess.run(pred, feed_dict)
+            print p[0]
+
+            p_rnd = np.int32(p[0])
+
+            cv2.imshow('GrayFrame',gray)
+            img[:] = (0,0,0) # clear
+            cv2.circle(img, (p_rnd[0], p_rnd[1]), 10, (255,255,255), -1)
+            cv2.imshow('Focus', img)
+            #Display the resulting frame
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+if __name__ == '__main__':
+    main()
